ca03fc7f933f83e8677876555c0c9eb8374ddc3c
nicolas <nicolas97fatima@gmail.com>
2022-02-12 13:21:49 -0300

    initial commit

---
+++ public/js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+    {
+      name: "I remember",
+      artist: "bbno$",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Hymn For The Weekend",
+      artist: "Alan Walker ft Coldplay",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Mr. Blue Sky",
+      artist: "Electric Light Orchestra",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Overwhelmed",
+      artist: "Royal & the Serpent",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "The Scientist",
+      artist: "Coldplay",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Secrets",
+      artist: "Tiësto & KSHMR feat. Vassy",
+      img: "music-6",
+      src: "music-6"
+    },
+    
+  ];
